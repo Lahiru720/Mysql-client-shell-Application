0500d6b92ffd0947f596fc324ca793f16c13062e
LahiruRJ <sanlahiru720@gmail.com>
2022-03-04 09:46:09 +0530

    Change LoginForm to JFX

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+    public PasswordField txtPassword;
+    public Button btnConnect;
+    public Button btnExit;
+    public TextField txtUserName;
+    public TextField txtPort;
+    public TextField txtHost;
+
+    public void btnExit_OnAction(ActionEvent event) {
+    }
+
+    public void btnConnect_OnAction(ActionEvent event) {
+    }
 }
